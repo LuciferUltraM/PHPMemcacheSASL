51d24ac2db9042265f66f4bc8b23eb556c51a8c4
LuciferUltraM <luciferultram@gmail.com>
2012-06-01 00:37:37 +0700

    Edit Example

--- MemcacheSASL.php
+++ MemcacheSASL.php
@@ -1,16 +1,8 @@
 <?php
-#
-#Memcache class with SASL support
-#
-#Memcache Binary Protocol
-#http://code.google.com/p/memcached/wiki/BinaryProtocolRevamped
-#
-#Memcache SASL Auth Protocol
-#http://code.google.com/p/memcached/wiki/SASLAuthProtocol
-#
-#PHP Memcached class
-#http://php.net/manual/en/class.memcached.php
-#
+
+# https://github.com/LuciferUltraM/PHPMemcacheSASL
+# 
+# Fork from https://github.com/ronnywang/PHPMemcacheSASL
 #
 
 class MemcacheSASL
--- example.php
+++ example.php
@@ -1,10 +1,31 @@
 <?php
-
 include('MemcacheSASL.php');
 
-$m = new MemcacheSASL;
-$m->addServer('mc7.ec2.northscale.net', '11211');
-$m->setSaslAuthData('username', 'password');
-var_dump($m->add('test', '123'));
-$m->delete('test');
+$memcached = new MemcacheSASL;
+$memcached->addServer('mc7.ec2.northscale.net', '11211');
+$memcached->setSaslAuthData('username', 'password');
 
+$key = 'test';
+$value = '123';
+$cache_time = 10;
+
+// add key & value
+var_dump($memcached->add($key, $value,$cache_time));
+
+//delete by key
+$memcached->delete($key);
+
+// flush all
+$memcached->flush();
+
+# Example
+
+if($memcached->get($key) != FALSE)
+{
+	echo $memcached->get($key);
+}
+else
+{
+	$memcached->add($key, $value,$cache_time);
+	$value
+}
